d0dbd7568330e533cbb60effa489cdf500708d62
Andrey <Andreikoman@gmail.com>
2020-06-03 20:36:29 +0300

    HW1 js

---
+++ scripts/task1.js
@@ -0,0 +1,15 @@
+        // Task 1
+
+    let str = 'Привет',
+        sum = 123,
+        num = 15.8,
+        flag = true,
+        txt = 'true';
+
+console.log ('Task 1');
+console.log (str, typeof str);
+console.log (sum, typeof sum);
+console.log (num, typeof num);
+console.log (flag, typeof flag);
+console.log (txt, typeof txt);
+console.log ('');
